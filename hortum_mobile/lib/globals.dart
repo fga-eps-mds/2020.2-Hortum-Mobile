0fc4cf8828c15c2bce6bab492054ebb9f2151a61
VitorLamego <vitorlamegoo@gmail.com>
2021-03-17 18:24:50 -0300

    #61 Adicionando comentários para explicar a utilização do dio

--- hortum_mobile/lib/globals.dart
+++ hortum_mobile/lib/globals.dart
@@ -1,6 +1,11 @@
 import 'package:dio/dio.dart';
 
-String url = 'http://localhost:8000';
+// Ip do usuário que está testando
+// Para isso, adicione o ip ao ALLOWED_HOSTS no back !!
+String url = 'http://127.0.0.1:8000/';
 
-Dio dio = new Dio(
-    new BaseOptions(baseUrl: url, connectTimeout: 5000, followRedirects: true));
+Dio dio = new Dio(new BaseOptions(
+  baseUrl: url,
+  connectTimeout: 5000,
+  followRedirects: true,
+));
