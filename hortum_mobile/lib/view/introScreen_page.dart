1b7ace1b573333c389e04a3b3882ba83c5e520dd
VitorLamego <vitorlamegoo@gmail.com>
2021-03-24 12:50:13 -0300

    #117 Adicionando tela de perfil

--- hortum_mobile/lib/view/introScreen_page.dart
+++ hortum_mobile/lib/view/introScreen_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hortum_mobile/view/profile/profile.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 import 'login/login_page.dart';
