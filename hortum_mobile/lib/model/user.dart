9b3a37a4728ebbb1441fc794c276079bbff44cc0
VitorLamego <vitorlamegoo@gmail.com>
2021-03-25 10:52:32 -0300

    #117 Tornando dinâmica informação apresentada na tela Meu Perfil de acordo com a model atualizada

---
+++ hortum_mobile/lib/model/user.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class User {
+  String tokenRefresh;
+  String tokenAccess;
+  String email;
+  String username;
+  String password;
+  bool isProductor;
+  final _storage = new FlutterSecureStorage();
+
+  User(
+      {this.tokenRefresh,
+      this.tokenAccess,
+      this.email,
+      this.username,
+      this.password,
+      this.isProductor});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        tokenRefresh: json['refresh'],
+        tokenAccess: json['access'],
+        email: json['email'],
+        username: json['username'],
+        password: json['password'],
+        isProductor: json['is_productor']);
+  }
+
+  Future writeSecureData(String key, String value) async {
+    var writeData = await _storage.write(key: key, value: value);
+    return writeData;
+  }
+
+  Future readSecureData(String key) async {
+    var readData = await _storage.read(key: key);
+    return readData;
+  }
+
+  Future deleteSecureData(String key) async {
+    var deleteData = await _storage.delete(key: key);
+    return deleteData;
+  }
+}
