4fdeed012ef8b0bbc2b663b89ba9b339ad24616e
vital14 <limavictor49@gmail.com>
2021-03-29 17:38:48 -0300

    #129 Componentizando e padronizando paginas principais
    Co-Authored-by: VitorLamego <vitorlamegoo@gmail.com>

---
+++ hortum_mobile/lib/views/home_customer/components/search.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  final TextEditingController controller;
+  const Search({@required this.controller, Key key}) : super(key: key);
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return TextFormField(
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.search),
+          hintText: "Pesquisar",
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.only(bottom: size.height * 0.018)),
+      cursorColor: Colors.black.withOpacity(0.2),
+      controller: widget.controller,
+      onChanged: (value) {
+        super.setState(() {});
+      },
+    );
+  }
+}
--- hortum_mobile/lib/views/home_customer/home_customer_page.dart
+++ hortum_mobile/lib/views/home_customer/home_customer_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hortum_mobile/components/announcement_box.dart';
+import 'package:hortum_mobile/components/announcements_data.dart';
 import 'package:hortum_mobile/components/footer.dart';
-import 'package:hortum_mobile/views/home_customer/components/categories.dart';
+import 'package:hortum_mobile/views/home_customer/components/carroussel.dart';
+
+import 'components/search.dart';
 
 class CustomerHomePage extends StatefulWidget {
   @override
@@ -21,50 +24,10 @@ class _CustomerHomePageState extends State<CustomerHomePage> {
           child: Column(
             children: [
               Container(
-                margin: EdgeInsets.only(top: size.height * 0.08),
-                padding: EdgeInsets.only(left: 10),
-                height: size.height * 0.25,
-                child: ListView.builder(
-                  itemCount: categories.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      padding: EdgeInsets.only(right: size.width * 0.01),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: ClipRRect(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(15)),
-                                child: Material(
-                                  child: InkWell(
-                                      child: Image.asset(
-                                          categories[index]['image_path'],
-                                          fit: BoxFit.fill,
-                                          height: size.height * 0.12,
-                                          width: size.height * 0.12)),
-                                ),
-                              ),
-                            ),
-                            Container(
-                                padding: EdgeInsets.only(top: 5),
-                                child: Text(
-                                  categories[index]['name'],
-                                  style: TextStyle(
-                                      fontFamily: 'Roboto-Bold',
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black),
-                                )),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+                  margin: EdgeInsets.only(top: size.height * 0.08),
+                  padding: EdgeInsets.only(left: 10),
+                  height: size.height * 0.25,
+                  child: Carroussel()),
               Container(
                   width: size.width * 0.8,
                   height: size.height * 0.04,
@@ -73,18 +36,8 @@ class _CustomerHomePageState extends State<CustomerHomePage> {
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
                       color: Colors.grey.withOpacity(0.4)),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Pesquisar",
-                        border: InputBorder.none,
-                        contentPadding:
-                            EdgeInsets.only(bottom: size.height * 0.018)),
-                    cursorColor: Colors.black.withOpacity(0.2),
+                  child: Search(
                     controller: _filter,
-                    onChanged: (value) {
-                      setState(() {});
-                    },
                   )),
               Container(
                 height: size.height * 0.45,
