ceb857da00324bd8d25075da8217365f1e6ffa31
brenno-silva <brenno.silva037@gmail.com>
2021-04-03 22:03:14 -0300

    #95 Adicionando data para excluir anuncio

---
+++ hortum_mobile/lib/data/announ_delete_backend.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:hortum_mobile/globals.dart';
+import 'package:http/http.dart' as http;
+
+class DeleteAnnounApi {
+  static Future deleteAnnoun(String anuncio) async {
+    String userAccessToken = await actualUser.readSecureData('token_access');
+    var url = 'http://$ip:8000/announcement/delete/$anuncio';
+    var header = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + userAccessToken,
+    };
+
+    var response = await http.delete(url, headers: header);
+    Map mapResponse = json.decode(response.body);
+    print(mapResponse.values);
+  }
+}
