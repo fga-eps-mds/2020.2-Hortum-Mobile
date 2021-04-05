eef466e00daf125a5cb6939b8d59e5bdd648c410
brenno-silva <brenno.silva037@gmail.com>
2021-04-05 19:40:51 -0300

    #94 Adicionando comunicaçao com o backend
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>
    Co-authored-by: vital14 <limavictor49@gmail.com>

---
+++ hortum_mobile/lib/data/announ_edit_backend.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:hortum_mobile/globals.dart';
+import 'package:http/http.dart' as http;
+
+class EditAnnounApi {
+  static Future editAnnoun(String nomeOriginal,
+      {String name,
+      String description,
+      double price,
+      String category,
+      bool inventory}) async {
+    String userAccessToken = await actualUser.readSecureData('token_access');
+    var url = 'http://$ip:8000/announcement/update/$nomeOriginal';
+    var header = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + userAccessToken,
+    };
+    Map params;
+    params.removeWhere((key, value) => value == null);
+
+    /*if (name != null) params.addAll({"name": name});
+    if (description != null) params.addAll({"description": description});
+    if (price != null) params.addAll({"price": price});
+    if (category != null) params.addAll({"category": category});
+    if (inventory != null) params.addAll({"inventory": inventory});*/
+
+    String _body = json.encode(params);
+
+    var response = await http.patch(url, headers: header, body: _body);
+    Map mapResponse = json.decode(response.body);
+    print(mapResponse.values);
+  }
+}
