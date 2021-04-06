da62336369e4b1a87d72c6714500f391c867eb72
brenno-silva <brenno.silva037@gmail.com>
2021-04-06 19:02:20 -0300

    #142 Adicionando comunicaçao com o backend
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>

---
+++ hortum_mobile/lib/data/auto_login_backend.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:hortum_mobile/globals.dart';
+import 'package:http/http.dart' as http;
+
+class AutomaticLoginAPI {
+  static Future<bool> automaticLogin() async {
+    //Trocar o IPLOCAL pelo ip de sua máquina
+    String userAccessToken = await actualUser.readSecureData('token_access');
+    var urlTest = 'http://$ip:8000/api/test_token/';
+    var urlRefresh = 'http://$ip:8000/login/refresh/';
+    var header = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + userAccessToken,
+    };
+
+    Map params = {"access": userAccessToken};
+    String _body = json.encode(params);
+    var response = await http.get(urlTest, headers: header);
+
+    if (response.statusCode != 200) {
+      response = await http.post(urlRefresh, body: _body);
+      String newToken = json.decode(response.body)['access'];
+      actualUser.updateToken(newToken);
+      header['Authorization'] = "Bearer " + newToken;
+      response = await http.get(urlTest, headers: header);
+    }
+
+    String username = json.decode(response.body)['user'];
+    bool isProductor = json.decode(response.body)['is_productor'];
+    actualUser.initAutoLogin(isProductor, username);
+
+    return isProductor;
+  }
+}
