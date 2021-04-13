6067e2e401c16281cc34ff263d41f97106ef6a86
Carlos Eduardo de Sousa Fiuza <carlosfiuza1234@gmail.com>
2021-04-13 12:46:56 -0300

    #92 Adicionando services view change password
    Co-authored-by: brenno-silva <brenno.silva037@gmail.com>

---
+++ hortum_mobile/lib/views/change_password/services/change_password_services.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:hortum_mobile/data/change_password_backend.dart';
+import 'package:hortum_mobile/globals.dart';
+import 'package:hortum_mobile/views/home_customer/home_customer_page.dart';
+import 'package:hortum_mobile/views/home_productor/home_productor_page.dart';
+
+class ChangeServices {
+  static Future changePassword(String actualPasswordForm,
+      String newPasswordForm, BuildContext context) async {
+    final actualPassword = actualPasswordForm;
+    final newPassword = newPasswordForm;
+    final user = actualUser;
+
+    var response =
+        await ChangePasswordAPI.changePassword(actualPassword, newPassword);
+    if (response == 'Senha incorreta!') {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            key: Key('senhaIncorreta'),
+            title: Text("Erro!"),
+            content: Text(
+              "Senha atual incorreta",
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16,
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  "OK",
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 22,
+                    color: Color.fromARGB(0xFF, 244, 156, 0),
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      );
+    } else {
+      if (user.isProductor) {
+        return Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return ProductorHomePage();
+        }));
+      } else {
+        return Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return CustomerHomePage();
+        }));
+      }
+    }
+  }
+}
