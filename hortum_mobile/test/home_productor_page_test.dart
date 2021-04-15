ae5252d48dfd2b2db0cadea1cc26adfa77c9e050
Matheus Pinheiro <matheuscvp12@gmail.com>
2021-04-15 00:42:56 -0300

    #157 adicionando teste da homepage de productor

--- hortum_mobile/test/home_productor_page_test.dart
+++ hortum_mobile/test/home_productor_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hortum_mobile/data/prod_log_data_backend.dart';
 import 'package:hortum_mobile/globals.dart';
 import 'package:hortum_mobile/views/home_productor/home_productor_page.dart';
 
@@ -20,5 +21,42 @@ main() {
     await tester.pump();
 
     expect(find.byKey(Key('removeAnnoun')), findsOneWidget);
+  });
+
+  test('Testing the informations of the productor logged', () {
+    ProdLoggedAnnounDataApi announProd = ProdLoggedAnnounDataApi();
+    dynamic element = {
+      "username": "victor",
+      "email": "victor@gmail.com",
+      "idPicture": "uma ai",
+      "announcements": {
+        "username": "victor",
+        "idPictureProductor": "uma ai",
+        "name": "folha Verde",
+        "type_of_product": "Alface",
+        "description": "nao aguento mais",
+        "price": 5.0,
+        "idPicture": "uma ai"
+      }
+    };
+    announProd.productor = element;
+
+    announProd.manipulateData();
+    dynamic result = {
+      "username": "victor",
+      "email": "victor@gmail.com",
+      "idPicture": "uma ai",
+      "announcements": {
+        "username": "victor",
+        "idPictureProductor": "uma ai",
+        "name": "folha Verde",
+        "type_of_product": "Alface",
+        "description": "nao aguento mais",
+        "price": 5.0,
+        "idPicture": "uma ai"
+      }
+    };
+
+    expect(result, announProd.productor);
   });
 }
