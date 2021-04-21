d499e194bc42c2c42ffe33afd5b5ca90c1f099b8
Carlos Eduardo de Sousa Fiuza <carlosfiuza1234@gmail.com>
2021-04-20 23:16:58 -0300

    #180 Criando components da view Reclamation

---
+++ hortum_mobile/lib/views/reclamation/components/photo_selecter_reclamation.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class PhotoSelecterReclamation extends StatefulWidget {
+  @override
+  _PhotoSelecterReclamationState createState() =>
+      _PhotoSelecterReclamationState();
+
+  const PhotoSelecterReclamation({Key key}) : super(key: key);
+}
+
+class _PhotoSelecterReclamationState extends State<PhotoSelecterReclamation> {
+  //File _image;
+  final picker = ImagePicker();
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      decoration: BoxDecoration(),
+      child: Container(
+        width: double.infinity,
+        height: size.height * 0.1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(
+                  bottom: size.height * 0.01, right: size.width * 0.6),
+              child: Text(
+                'Imagem',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Comfortaa-Regular',
+                    letterSpacing: -0.33,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffC4C4C4).withOpacity(0.5),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: MaterialButton(
+                height: size.height * 0.05,
+                minWidth: size.width * 0.82,
+                child: Text('+ Adicionar imagem'),
+                onPressed: () async {
+                  final pickedFile =
+                      await picker.getImage(source: ImageSource.gallery);
+
+                  setState(() {
+                    if (pickedFile != null) {
+                      //  _image = File(pickedFile.path);
+                    } else {
+                      print('No image selected.');
+                    }
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ hortum_mobile/lib/views/reclamation/components/reclamation_form.dart
@@ -0,0 +1,109 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:hortum_mobile/components/custom_desc_field.dart';
+import 'package:hortum_mobile/components/form_field.dart';
+import 'package:hortum_mobile/components/form_validation.dart';
+import 'package:hortum_mobile/views/reclamation/components/photo_selecter_reclamation.dart';
+import 'package:hortum_mobile/views/reclamation/services/reclamation_services.dart';
+
+class ReclamationForm extends StatefulWidget {
+  final Dio dio;
+  final TextEditingController name;
+  final TextEditingController description;
+  final String emailProductor;
+
+  const ReclamationForm(
+      {this.dio, this.name, this.description, this.emailProductor, Key key})
+      : super(key: key);
+  @override
+  _ReclamationFormState createState() =>
+      _ReclamationFormState(name: name, description: description);
+}
+
+class _ReclamationFormState extends State<ReclamationForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController name;
+  final TextEditingController description;
+
+  _ReclamationFormState({this.name, this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height * 0.7,
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.45,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: CustomFormField(
+                      suffixIcon: false,
+                      obscureText: false,
+                      labelText: 'Seu nome',
+                      controller: name,
+                      icon: null,
+                      validator: FormValidation.validateName,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, right: 165),
+                    child: Text(
+                      'Descrição do ocorrido',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Comfortaa-Regular',
+                          letterSpacing: -0.33,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5, bottom: 20),
+                    child: CustomDescField(
+                      controller: description,
+                      validator: FormValidation.validateDescription,
+                    ),
+                  ),
+                  PhotoSelecterReclamation(),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: size.height * 0.2,
+            child: MaterialButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  ReclamationServices.registerReclamation(widget.dio, name.text,
+                      description.text, widget.emailProductor, context);
+                }
+              },
+              child: Container(
+                width: size.width * 0.5,
+                height: size.height * 0.04,
+                margin: EdgeInsets.only(top: size.height / 100),
+                decoration: BoxDecoration(
+                  color: Color(0xfff46a6a),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text("ENVIAR",
+                      style: TextStyle(
+                          color: Colors.white, fontFamily: 'Roboto-Bold')),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
