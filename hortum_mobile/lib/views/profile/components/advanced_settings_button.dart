db69ff6566912eb917e12c3ec12fe7cd377d2f72
brenno-silva <brenno.silva037@gmail.com>
2021-04-25 18:06:35 -0300

    #89 Refatorando ProfilePage e componentes

--- hortum_mobile/lib/views/profile/components/advanced_settings_button.dart
+++ hortum_mobile/lib/views/profile/components/advanced_settings_button.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:hortum_mobile/globals.dart';
-import 'package:hortum_mobile/views/login/login_page.dart';
+import 'package:hortum_mobile/views/advanced_settings/advanced_settings_page.dart';
 
-class LogoutButton extends StatelessWidget {
+class AdvancedSettingsButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
+      padding: EdgeInsets.only(top: 6),
       onPressed: () {
-        actualUser.deleteUser();
         Navigator.pushAndRemoveUntil(
             context,
-            MaterialPageRoute(builder: (context) => LoginPage()),
+            MaterialPageRoute(builder: (context) => AdvancedSettingsPage()),
             (route) => route.isCurrent);
       },
       child: Text(
-        "Sair",
+        "Avançado",
         style: TextStyle(
             fontSize: 15.0,
             decoration: TextDecoration.underline,
