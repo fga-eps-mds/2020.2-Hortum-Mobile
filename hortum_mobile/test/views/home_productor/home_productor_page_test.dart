5381e9aff753857dc531f6e4bc14939273db2d40
VitorLamego <vitorlamegoo@gmail.com>
2021-04-26 17:39:45 -0300

    #193 Removendo imports não utilizados

--- hortum_mobile/test/views/home_productor/home_productor_page_test.dart
+++ hortum_mobile/test/views/home_productor/home_productor_page_test.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hortum_mobile/data/prod_log_data_backend.dart';
 import 'package:hortum_mobile/globals.dart';
 import 'package:hortum_mobile/views/home_productor/home_productor_page.dart';
 import 'package:mockito/mockito.dart';
