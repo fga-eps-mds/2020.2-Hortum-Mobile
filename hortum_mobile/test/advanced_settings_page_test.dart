3e659c8cc3aec75bc66330dbf4b200863b87cbbd
brenno-silva <brenno.silva037@gmail.com>
2021-04-25 22:14:39 -0300

    #89 Adicionando testes para os botoes na AdvancedSettingsPage

--- hortum_mobile/test/advanced_settings_page_test.dart
+++ hortum_mobile/test/advanced_settings_page_test.dart
@@ -22,5 +22,35 @@ main() {
       expect(find.text('Excluir conta'), findsOneWidget);
       expect(find.text('Sair'), findsOneWidget);
     });
+    testWidgets('Testing the LogoutButton', (WidgetTester tester) async {
+      actualUser.isProductor = true;
+      await tester.pumpWidget(makeTestable());
+      await tester.pump();
+      await tester.tap(find.text('Sair'));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('ENTRAR'), findsOneWidget);
+    });
+
+    testWidgets('Testing the DeleteAccountButton', (WidgetTester tester) async {
+      actualUser.isProductor = true;
+      await tester.pumpWidget(makeTestable());
+      await tester.pump();
+      await tester.tap(find.text('Excluir conta'));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('EXCLUIR CONTA'), findsOneWidget);
+    });
+
+    testWidgets('Testing the ChangePasswordButton',
+        (WidgetTester tester) async {
+      actualUser.isProductor = true;
+      await tester.pumpWidget(makeTestable());
+      await tester.pump();
+      await tester.tap(find.text('Mudar senha'));
+      await tester.pump();
+      await tester.pump();
+      expect(find.text('MUDAR SENHA'), findsOneWidget);
+    });
   });
 }
