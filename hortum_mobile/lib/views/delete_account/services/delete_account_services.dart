9720dd7926e06ce72ba95c7beff6841753d209fa
brenno-silva <brenno.silva037@gmail.com>
2021-04-27 11:40:48 -0300

    #89 Corrigindo views

--- hortum_mobile/lib/views/delete_account/services/delete_account_services.dart
+++ hortum_mobile/lib/views/delete_account/services/delete_account_services.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:hortum_mobile/data/user_delete_backend.dart';
+import 'package:hortum_mobile/data/user_data_backend.dart';
 import 'package:hortum_mobile/globals.dart';
 import 'package:hortum_mobile/views/login/login_page.dart';
 
 Future<void> deleteUser(Dio dio, String password, BuildContext context) async {
-  final DeleteUserAPI deleteData = new DeleteUserAPI(dio);
+  final UserAPI deleteData = new UserAPI(dio);
   var response = await deleteData.deleteUser(password);
   if (response.statusCode != 204) {
     showDialog(
