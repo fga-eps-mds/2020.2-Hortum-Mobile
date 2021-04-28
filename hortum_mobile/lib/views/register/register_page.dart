45304f7ea30793259fe4462292fec51dbcb1ee7f
VitorLamego <vitorlamegoo@gmail.com>
2021-04-28 02:46:59 -0300

    #27 Trocando o pacote http para Dio na classe RegisterApi

--- hortum_mobile/lib/views/register/register_page.dart
+++ hortum_mobile/lib/views/register/register_page.dart
@@ -1,17 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:hortum_mobile/components/photo_select.dart';
 import 'package:hortum_mobile/views/register/components/register_form.dart';
 
 class RegisterPage extends StatefulWidget {
-  final bool _isProductor;
-  RegisterPage(this._isProductor);
+  final bool isProductor;
+  final Dio dio;
+
+  const RegisterPage({@required this.isProductor, this.dio, Key key})
+      : super(key: key);
   @override
-  _RegisterPageState createState() => _RegisterPageState(_isProductor);
+  _RegisterPageState createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  final bool _isProductor;
-  _RegisterPageState(this._isProductor);
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
@@ -23,7 +25,7 @@ class _RegisterPageState extends State<RegisterPage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               PhotoSelecter(title: 'CADASTRO'),
-              RegisterForm(_isProductor),
+              RegisterForm(isProductor: widget.isProductor, dio: widget.dio),
             ],
           ),
         ),
