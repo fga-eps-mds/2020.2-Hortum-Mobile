f73e819e6df8ab2a211ecfbe2ba3e3e7891717f7
VitorLamego <vitorlamegoo@gmail.com>
2021-04-29 11:14:10 -0300

    #161 Refatorando design dos componentes

--- hortum_mobile/lib/views/announcement_details/components/localization_container.dart
+++ hortum_mobile/lib/views/announcement_details/components/localization_container.dart
@@ -12,99 +12,109 @@ class _LocalizationContainerState extends State<LocalizationContainer> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-        margin: EdgeInsets.only(bottom: size.height * 0.03),
-        width: size.width * 0.9,
-        height: size.height * 0.1496,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            border: Border.all(color: Colors.black.withOpacity(0.25)),
-            boxShadow: [
-              BoxShadow(
-                  blurRadius: 4,
-                  color: Colors.black.withOpacity(0.35),
-                  spreadRadius: 0,
-                  offset: Offset(0, 4))
-            ]),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(
-                    left: size.width * 0.05,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+      child: Container(
+          padding: EdgeInsets.only(top: 10),
+          margin: EdgeInsets.only(bottom: size.height * 0.03),
+          width: size.width * 0.9,
+          height: size.height * 0.2,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              border: Border.all(color: Colors.black.withOpacity(0.25)),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 4,
+                    color: Colors.black.withOpacity(0.35),
+                    spreadRadius: 0,
+                    offset: Offset(0, 4))
+              ]),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: size.width * 0.05,
+                    ),
+                    height: size.height * 0.03,
+                    width: size.width * 0.06,
+                    child: Icon(
+                      Icons.place,
+                      size: 20,
+                      color: Colors.black,
+                    ),
                   ),
-                  height: size.height * 0.03,
-                  width: size.width * 0.06,
-                  child: Icon(
-                    Icons.place,
-                    size: 25,
-                    color: Colors.black,
-                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: size.width * 0.02),
+                    child: Text(
+                      "Localizações",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontFamily: 'Comfortaa-Regular',
+                          letterSpacing: -0.33,
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(right: size.width * 0.2),
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                  widget.localization,
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                      fontSize: 15,
+                      fontFamily: 'Comfortaa-Regular',
+                      letterSpacing: -0.33,
+                      fontWeight: FontWeight.w300),
                 ),
-                Container(
-                  child: Text(
-                    "Localização",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontFamily: 'Comfortaa-Regular',
-                        letterSpacing: -0.33,
-                        fontWeight: FontWeight.w300),
-                  ),
-                )
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.only(right: size.width * 0.2),
-              child: Text(
-                widget.localization,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Comfortaa-Regular',
-                    letterSpacing: -0.33,
-                    fontWeight: FontWeight.w300),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(right: size.width * 0.2),
-              child: Text(
-                widget.localization,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Comfortaa-Regular',
-                    letterSpacing: -0.33,
-                    fontWeight: FontWeight.w300),
+              Container(
+                margin: EdgeInsets.only(right: size.width * 0.2),
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                  widget.localization,
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                      fontSize: 15,
+                      fontFamily: 'Comfortaa-Regular',
+                      letterSpacing: -0.33,
+                      fontWeight: FontWeight.w300),
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(right: size.width * 0.2),
-              child: Text(
-                widget.localization,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Comfortaa-Regular',
-                    letterSpacing: -0.33,
-                    fontWeight: FontWeight.w300),
+              Container(
+                margin: EdgeInsets.only(right: size.width * 0.2),
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                  widget.localization,
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                      fontSize: 15,
+                      fontFamily: 'Comfortaa-Regular',
+                      letterSpacing: -0.33,
+                      fontWeight: FontWeight.w300),
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(right: size.width * 0.2),
-              child: Text(
-                widget.localization,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontFamily: 'Comfortaa-Regular',
-                    letterSpacing: -0.33,
-                    fontWeight: FontWeight.w300),
+              Container(
+                margin: EdgeInsets.only(right: size.width * 0.2),
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                  widget.localization,
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.7),
+                      fontSize: 15,
+                      fontFamily: 'Comfortaa-Regular',
+                      letterSpacing: -0.33,
+                      fontWeight: FontWeight.w300),
+                ),
               ),
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
--- hortum_mobile/lib/views/announcement_details/components/price_container.dart
+++ hortum_mobile/lib/views/announcement_details/components/price_container.dart
@@ -11,59 +11,59 @@ class _PriceContainerState extends State<PriceContainer> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      margin:
-          EdgeInsets.only(bottom: size.height * 0.04, top: size.height * 0.01),
-      width: size.width * 0.9,
-      height: size.height * 0.04,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-          border: Border.all(color: Colors.black.withOpacity(0.25)),
-          boxShadow: [
-            BoxShadow(
-                blurRadius: 4,
-                color: Colors.black.withOpacity(0.35),
-                spreadRadius: 0,
-                offset: Offset(0, 4))
-          ]),
-      child: Row(children: [
-        Container(
-          margin: EdgeInsets.only(
-              left: size.width * 0.05, right: size.width * 0.02),
-          height: size.height * 0.03,
-          width: size.width * 0.06,
-          child: Icon(
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+      child: Container(
+        margin: EdgeInsets.only(
+            bottom: size.height * 0.04, top: size.height * 0.05),
+        width: size.width * 0.9,
+        height: size.height * 0.06,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            border: Border.all(color: Colors.black.withOpacity(0.25)),
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 4,
+                  color: Colors.black.withOpacity(0.35),
+                  spreadRadius: 0,
+                  offset: Offset(0, 4))
+            ]),
+        child: Row(children: [
+          Padding(padding: EdgeInsets.only(left: size.width * 0.05)),
+          Icon(
             Icons.credit_card_outlined,
             size: 25,
             color: Colors.black,
           ),
-        ),
-        Container(
-          child: Text(
-            "Preço:",
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 17,
-                fontFamily: 'Comfortaa-Regular',
-                letterSpacing: -0.33,
-                fontWeight: FontWeight.w300),
+          Container(
+            padding: EdgeInsets.only(left: 5),
+            child: Text(
+              "Preço:",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontFamily: 'Comfortaa-Regular',
+                  letterSpacing: -0.33,
+                  decoration: TextDecoration.underline,
+                  fontWeight: FontWeight.w400),
+            ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-              left: size.width * 0.02, right: size.width * 0.02),
-          child: Text(
-            widget.price,
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 17,
-                fontFamily: 'Comfortaa-Regular',
-                letterSpacing: -0.33,
-                fontWeight: FontWeight.w300),
-          ),
-        )
-      ]),
+          Container(
+            margin: EdgeInsets.only(
+                left: size.width * 0.02, right: size.width * 0.02),
+            child: Text(
+              widget.price,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontFamily: 'Comfortaa-Regular',
+                  letterSpacing: -0.33,
+                  fontWeight: FontWeight.w400),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
