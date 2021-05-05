46d2985cd50e4c78d2fb8c73cc8f76d80b88a403
VitorLamego <vitorlamegoo@gmail.com>
2021-05-05 16:35:02 -0300

    #209 Adicionando imagem ao cadastro de usuário
    Co-authored-by: vital14 <limavictor49@gmail.com>

--- hortum_mobile/lib/data/register_backend.dart
+++ hortum_mobile/lib/data/register_backend.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:hortum_mobile/globals.dart';
@@ -6,6 +5,7 @@ import 'package:hortum_mobile/views/login/login_page.dart';
 
 class RegisterApi {
   Dio dio;
+
   RegisterApi([Dio client]) {
     if (client == null)
       this.dio = Dio();
@@ -21,22 +21,25 @@ class RegisterApi {
 
     var header = {"Content-Type": "application/json"};
 
-    Map params = {
-      "user": {
-        "username": username,
-        "email": email,
-        "password": password,
-      },
-    };
+    String filename = profile_picture.path.split('/').last;
+    var params = FormData.fromMap({
+      "user.username": username,
+      "user.email": email,
+      "user.password": password,
+      "user.profile_picture":
+          await MultipartFile.fromFile(profile_picture.path, filename: filename)
+    });
 
-    String _body = json.encode(params);
     if (isProductor == false) {
-      response = await this
-          .dio
-          .post(urlCustomer, data: _body, options: Options(headers: header));
+      response = await this.dio.post(urlCustomer,
+          data: params,
+          options:
+              Options(headers: header, contentType: 'multipart/form-data'));
     } else {
       response = await dio.post(urlProductor,
-          data: _body, options: Options(headers: header));
+          data: params,
+          options:
+              Options(headers: header, contentType: 'multipart/form-data'));
     }
     if (response.statusCode == 201) {
       Navigator.push(context, MaterialPageRoute(builder: (context) {
--- hortum_mobile/lib/views/register/components/register_form.dart
+++ hortum_mobile/lib/views/register/components/register_form.dart
@@ -30,88 +30,88 @@ class _RegisterFormState extends State<RegisterForm> {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-          height: size.height * 0.6,
-          decoration: new BoxDecoration(
-              gradient: LinearGradient(
-                  begin: FractionalOffset.bottomCenter,
-                  end: FractionalOffset.topCenter,
-                  colors: [
-                    Color(0xFF90ED80).withOpacity(0.18),
-                    Color(0xFF56C378).withOpacity(0.6),
-                  ],
-                  stops: [
-                    0.0,
-                    1.0
-                  ]),
-              borderRadius: BorderRadius.only(
-                topLeft: const Radius.circular(40.0),
-                topRight: const Radius.circular(40.0),
-              )),
-          child: Center(
-              child: Form(
-                  key: _formKey,
-                  child: Container(
-                    width: size.width * 0.75,
-                    margin: EdgeInsets.only(top: size.height * 0.02),
+        height: size.height * 0.6,
+        decoration: new BoxDecoration(
+            gradient: LinearGradient(
+                begin: FractionalOffset.bottomCenter,
+                end: FractionalOffset.topCenter,
+                colors: [
+                  Color(0xFF90ED80).withOpacity(0.18),
+                  Color(0xFF56C378).withOpacity(0.6),
+                ],
+                stops: [
+                  0.0,
+                  1.0
+                ]),
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(40.0),
+              topRight: const Radius.circular(40.0),
+            )),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Container(
+              width: size.width * 0.75,
+              margin: EdgeInsets.only(top: size.height * 0.02),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: size.height * 0.42,
+                    margin: EdgeInsets.only(bottom: size.height * 0.02),
                     child: Column(
-                      children: <Widget>[
-                        Container(
-                          height: size.height * 0.42,
-                          margin: EdgeInsets.only(bottom: size.height * 0.02),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              CustomFormField(
-                                suffixIcon: false,
-                                controller: _name,
-                                labelText: 'Nome',
-                                icon: Icon(Icons.face, color: Colors.black),
-                                validator: FormValidation.validateName,
-                              ),
-                              CustomFormField(
-                                suffixIcon: false,
-                                labelText: 'E-mail',
-                                controller: _email,
-                                icon: Icon(Icons.email_outlined,
-                                    color: Colors.black),
-                                validator: FormValidation.validateEmail,
-                              ),
-                              CustomFormField(
-                                suffixIcon: false,
-                                obscureText: true,
-                                labelText: 'Senha',
-                                controller: _password,
-                                icon:
-                                    Icon(Icons.lock_open, color: Colors.black),
-                                validator: FormValidation.validatePassword,
-                              ),
-                              CustomFormField(
-                                suffixIcon: false,
-                                obscureText: true,
-                                labelText: 'Confirmar Senha',
-                                controller: _confirmPassword,
-                                icon:
-                                    Icon(Icons.lock_open, color: Colors.black),
-                                validator: (value) {
-                                  return FormValidation.validateConfirmPassword(
-                                      _password.text, value);
-                                },
-                              ),
-                            ],
-                          ),
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CustomFormField(
+                          suffixIcon: false,
+                          controller: _name,
+                          labelText: 'Nome',
+                          icon: Icon(Icons.face, color: Colors.black),
+                          validator: FormValidation.validateName,
                         ),
-                        ConfirmButton(
-                            labelButton: "CADASTRAR",
-                            colorButton: Color(0xFF81B622),
-                            onClickAction: () {
-                              if (_formKey.currentState.validate()) {
-                                registerApi.register(_name.text, _email.text,
-                                    _password.text, _isProductor, context);
-                              }
-                            }),
+                        CustomFormField(
+                          suffixIcon: false,
+                          labelText: 'E-mail',
+                          controller: _email,
+                          icon: Icon(Icons.email_outlined, color: Colors.black),
+                          validator: FormValidation.validateEmail,
+                        ),
+                        CustomFormField(
+                          suffixIcon: false,
+                          obscureText: true,
+                          labelText: 'Senha',
+                          controller: _password,
+                          icon: Icon(Icons.lock_open, color: Colors.black),
+                          validator: FormValidation.validatePassword,
+                        ),
+                        CustomFormField(
+                          suffixIcon: false,
+                          obscureText: true,
+                          labelText: 'Confirmar Senha',
+                          controller: _confirmPassword,
+                          icon: Icon(Icons.lock_open, color: Colors.black),
+                          validator: (value) {
+                            return FormValidation.validateConfirmPassword(
+                                _password.text, value);
+                          },
+                        ),
                       ],
                     ),
-                  )))),
+                  ),
+                  ConfirmButton(
+                      labelButton: "CADASTRAR",
+                      colorButton: Color(0xFF81B622),
+                      onClickAction: () {
+                        if (_formKey.currentState.validate()) {
+                          registerApi.register(_name.text, _email.text,
+                              _password.text, _isProductor, context);
+                        }
+                      }),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
