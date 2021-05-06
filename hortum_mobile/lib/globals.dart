b63fb816547cfabcab8c84820f1f6aad250c5fad
VitorLamego <vitorlamegoo@gmail.com>
2021-05-05 21:16:28 -0300

    #209 Adicionando controller global

--- hortum_mobile/lib/globals.dart
+++ hortum_mobile/lib/globals.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:hortum_mobile/model/user.dart';
+import 'package:hortum_mobile/views/profile/services/add_picture_controller.dart';
 
 User actualUser = User();
 String ip = 'IPLOCAL';
 File profile_picture;
 List<File> announImages = [];
+AddPictureController controllerPicture = AddPictureController();
