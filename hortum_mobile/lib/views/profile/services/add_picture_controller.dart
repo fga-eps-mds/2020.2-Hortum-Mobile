73585014c70da728f6ffa8c87e7a77bc552bc27e
VitorLamego <vitorlamegoo@gmail.com>
2021-05-07 19:29:30 -0300

    #209 Adicionando controller para editar as imagens de um anúncio

--- hortum_mobile/lib/views/profile/services/add_picture_controller.dart
+++ hortum_mobile/lib/views/profile/services/add_picture_controller.dart
@@ -6,4 +6,9 @@ class AddPictureController {
   final newPictureNotifier = ValueNotifier<File>(null);
   File get editPicture => newPictureNotifier.value;
   set currentPage(File value) => newPictureNotifier.value = value;
+
+  final newPictureAnnounNotifier = ValueNotifier<File>(null);
+  File get annouPicture => newPictureAnnounNotifier.value;
+  set currentAnnounPicture(File value) =>
+      newPictureAnnounNotifier.value = value;
 }
