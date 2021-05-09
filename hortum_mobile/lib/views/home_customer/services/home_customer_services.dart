76287f0b446bb90e4ac1b5ed6004dcba45a18929
brenno-silva <brenno.silva037@gmail.com>
2021-05-09 19:49:21 -0300

    #99/#100 Corrigindo if

--- hortum_mobile/lib/views/home_customer/services/home_customer_services.dart
+++ hortum_mobile/lib/views/home_customer/services/home_customer_services.dart
@@ -4,12 +4,10 @@ import 'package:hortum_mobile/data/productors_data_backend.dart';
 class HomeCustomerServices {
   static populateData(String type, String filter, AnnouncementsApi announData,
       ProductorsDataApi productorsData) async {
-    if (type == "name") {
-      await announData.getAnnoun(filter, type);
-    } else if (type == "localizations__adress") {
-      await announData.getAnnoun(filter, type);
-    } else {
+    if (type == "Produc") {
       await productorsData.getProductors(filter);
+    } else {      
+      await announData.getAnnoun(filter, type);
     }
   }
 }
